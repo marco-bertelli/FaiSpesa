9f090d5230d53d987c7785a766eaa18c89ef4747
marco-bertelli <marcobert37@gmail.com>
2020-03-31 11:10:28 +0200

    login funzionante

--- faiSpesa/src/app/app-routing.module.ts
+++ faiSpesa/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-    //{ path: 'login', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
     //{ path: 'game-list', component: GameListComponent, canActivate: [MyRouteGuardService]},
    // { path: 'game-detail/:id', component: GameDetailComponent, canActivate: [MyRouteGuardService]},
