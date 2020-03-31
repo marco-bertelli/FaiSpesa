a115af294bdd72c579566ae84670e14226ead994
marco-bertelli <marcobert37@gmail.com>
2020-03-31 11:34:57 +0200

    routeguard su login

--- faiSpesa/src/app/app-routing.module.ts
+++ faiSpesa/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
+import { EditRouteGuard } from './services/guard/edit-route-guard.service';
 
 
 const routes: Routes = [
-    { path: 'login', component: LoginComponent },
+    { path: 'login', component: LoginComponent,canActivate: [EditRouteGuard] },
     { path: 'home', component: HomeComponent },
     //{ path: 'game-list', component: GameListComponent, canActivate: [MyRouteGuardService]},
    // { path: 'game-detail/:id', component: GameDetailComponent, canActivate: [MyRouteGuardService]},
