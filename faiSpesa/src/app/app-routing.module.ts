d798e6c228e8cf65c333f6561774b826786bd899
marco-bertelli <marcobert37@gmail.com>
2020-03-31 10:27:05 +0200

    routing funzionante

--- faiSpesa/src/app/app-routing.module.ts
+++ faiSpesa/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    //{ path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    //{ path: 'game-list', component: GameListComponent, canActivate: [MyRouteGuardService]},
+   // { path: 'game-detail/:id', component: GameDetailComponent, canActivate: [MyRouteGuardService]},
+   // { path: 'modifica', component:ModificaComponent, canActivate: [EditRouteGuard]},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  
+ }
