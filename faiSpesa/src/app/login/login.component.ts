8e5371e9cb2cf65ffd42e77d6e2657afc68156ed
marco-bertelli <marcobert37@gmail.com>
2020-03-31 10:56:06 +0200

    login(da testare)

---
+++ faiSpesa/src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginServiceService } from '../services/login-service.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private listaUtenti: LoginServiceService,private router: Router) {
+   }
+
+  ngOnInit(): void {
+  }
+
+  login(username:string, password:string){
+    
+    this.listaUtenti.eseguiLogin(username,password);
+    
+  }
+
+  loginS(username:string, password:string){
+    //nei pulsanti/altro metodo
+    sessionStorage.setItem('user', username);
+    sessionStorage.setItem('password', password);
+
+    this.listaUtenti.eseguiLoginS();
+  }
+
+}
---
+++ faiSpesa/src/app/services/login-service.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user.interface';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  
+
+  private gameList:User[]=[
+    {username:"marco",password:"prosciutto",admin:true},
+    {username:"carlo",password:"negro",admin:false},
+  ]
+  
+  constructor(private router: Router){
+
+  }
+
+  accesso(username:string,password:string):boolean{
+
+    let controllo=false;
+    
+    this.gameList.forEach(element => {
+      if(element.username===username && element.password===password){
+        //gestione admin
+        element.admin === true ? sessionStorage.setItem('privilege','admin') : sessionStorage.setItem('privilege','user')
+        
+        controllo=true;
+      }
+    });
+    
+    return controllo;  
+  }
+
+  eseguiLogin(username:string,password:string){
+
+    if(this.accesso(username,password)){
+      //se giusto memorizza con pass
+      sessionStorage.setItem('user', username);
+
+      this.router.navigateByUrl("/home");
+    }
+    else console.log("non funzionante")
+  }
+  //con sessioni
+  eseguiLoginS(){
+   let username=sessionStorage.getItem('user');
+   let password=sessionStorage.getItem('password');
+    
+    if(this.accesso(username,password)){
+      this.router.navigateByUrl("/home");
+    }
+  }
+
+ 
+}
