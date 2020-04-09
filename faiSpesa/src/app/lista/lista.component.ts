f3145c97d19cdacb20305fc534de53fb828344b4
marco-bertelli <marcobert37@gmail.com>
2020-04-09 14:59:43 +0200

    inizioForm

--- faiSpesa/src/app/lista/lista.component.ts
+++ faiSpesa/src/app/lista/lista.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ListServiceService } from '../services/lista/list-service.service';
 import { List } from '../models/list-interface';
 import { Prodotto } from '../models/prodotto-interface';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,15 +13,32 @@ import { Router } from '@angular/router';
 export class ListaComponent implements OnInit {
   utente=sessionStorage.getItem('user');
   lista:List[];
+  crea=false;
+  newSpesa:FormGroup;
 
-  constructor(private listService:ListServiceService,private router: Router) {
+
+  constructor(private listService:ListServiceService,private router: Router,private fb: FormBuilder) {
     this.lista=listService.getLista();
+    this.newSpesa=this.fb.group({
+      username:sessionStorage.getItem('user'),
+      nome:'',
+    });
    }
-
+  
   ngOnInit(): void {
   }
+
   apriD(id){
     this.router.navigate(['/dettaglio',id]);
   }
+  //metodo per visualizzare la form
+  visualizzaForm(){
+    this.crea=true;
+  }
+  //metodo per creare
+  onSubmit(primiValori){
+
+  }
+
 
 }
